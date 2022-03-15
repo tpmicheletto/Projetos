b6aad890d1341af136c2031cb81033238c61850c
tpmicheletto <t.hyago@hotmail.com>
2022-03-14 22:19:50 -0300

    teste

---
+++ Fichario/js.js
@@ -0,0 +1,10 @@
+// string
+
+const namef = 'thiago';
+const nomed = 'micheletto';
+
+console.log = ('meu nome é' + namef + ' ' + nomed);
+console.log = (`meu nome  é ${namef} ${nomed}`);
+
+const nome = ('joao,lucas,thiago,carol');
+console.log =(nome.split(","));
