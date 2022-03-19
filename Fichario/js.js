229033f34cc8e52011cdd8a481606ca356750853
tpmicheletto <t.hyago@hotmail.com>
2022-03-19 00:53:54 -0300

    finalizaçãop

--- Fichario/js.js
+++ Fichario/js.js
@@ -2,6 +2,7 @@ const botao = document.querySelector('#enviar');
 const nome = document.querySelector("#nome")
 const email = document.querySelector("#email")
 const erroMsg = document.querySelector('.mensagem')
+const item = document.querySelector("#usuario")
 
 botao.addEventListener("click", function(e){
     e.preventDefault();
@@ -11,5 +12,17 @@ botao.addEventListener("click", function(e){
 
     if (nomeValue === "" || emailValue ===""){
         erroMsg.innerHTML = 'Por favor preencha todos os campos.'
+        erroMsg.classList = "erro"
+        setTimeout(()=>{
+            erroMsg.textContent = "";
+            erroMsg.classList = "";
+        }, 2500)
+        return;
     }
+    const lista = document.createElement("li");
+    lista.classList = "item";
+    lista.innerHTML = `Nome: ${nomeValue}<br/>E-mail: ${emailValue}`
+    nome.value = ""
+    email.value = ""
+    item.appendChild(lista)
 });
